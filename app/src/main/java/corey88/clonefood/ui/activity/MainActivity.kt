8f3dd591c75071a843f08b58a814912c5cd678df
itscorey <luke8.co@gmail.com>
2018-05-05 13:54:43 -0300

    Adds UI's flags to make status bar transparent and possible to draw behind it

--- app/src/main/java/corey88/clonefood/ui/activity/MainActivity.kt
+++ app/src/main/java/corey88/clonefood/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package corey88.clonefood.ui.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -10,15 +11,24 @@ import corey88.clonefood.ui.fragment.MainFragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // We need to use those TAG cause if we don't our layout will not fill the screen.
+        // Then with the use of statusBarColor as @transparent we will create the same as iFood app!
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            window.decorView.systemUiVisibility += View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+
     }
 
 }
