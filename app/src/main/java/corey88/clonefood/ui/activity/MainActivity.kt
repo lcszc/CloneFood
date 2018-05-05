cf42b162cee37c825718953cfbac26ad9391e73b
itscorey <luke8.co@gmail.com>
2018-05-05 01:05:11 -0300

    Adds rounded layout effect

--- app/src/main/java/corey88/clonefood/ui/activity/MainActivity.kt
+++ app/src/main/java/corey88/clonefood/ui/activity/MainActivity.kt
@@ -2,12 +2,16 @@ package corey88.clonefood.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import corey88.clonefood.R
 import corey88.clonefood.ui.fragment.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
